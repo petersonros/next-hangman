aff7aafe3aac0fd1f615e82d089d1ad25d7c06bb
peterson <petersonrobertodossantos@gmail.com>
2024-07-31 23:12:49 -0300

    configurações iniciais

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+const Header: React.FC = () => (
+  <Head>
+    <title>Hangman Game</title>
+    <meta name="description" content="Hangman Game using Next.js" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+);
+
+export default Header;
