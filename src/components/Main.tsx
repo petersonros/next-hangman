7d69b848fec7e385c26aa3d221652e9a1eae7519
Peterson <petersonros@hotmail.com>
2025-08-07 20:13:42 -0300

    feat: substitui projeto antigo por novo jogo da forca em Next.js

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css';
+
+const Main: React.FC = () => (
+  <main className={styles.main}>
+    <h1 className={styles.title}>Hangman Game</h1>
+    {/* Adicione aqui o conteúdo do seu index.html */}
+  </main>
+);
+
+export default Main;
